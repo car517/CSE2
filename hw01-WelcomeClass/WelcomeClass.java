9c5a3f45a829ffc676f909cd8e8da6e6f45417a4
car517 <car517@lehigh.edu>
2014-08-31 21:08:30 +0000

    updated WelcomeClass

---
+++ hw01-WelcomeClass/WelcomeClass.java
@@ -0,0 +1,34 @@
+//////////////////////////////////
+//Carlos Rosario
+//CSE 002 Section 111
+//Welcome Class Java Program
+//August 31 2014
+//
+//First Compile The Program
+//      javac WelcomeClass.java
+//Run The Program
+//      java WelcomeClass//
+
+//  Define A Class
+public class WelcomeClass{
+
+// Add Main Method
+    public static void main(String[] args) {
+    
+// Print The Statement With Box Around It
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-C--A--R--5--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("My favorite car is the Lamborghini Aventador. I probably would not own one though because it costs 70,000 dollars to replace all four tires.");
+    
+    }
+}
+
+    
+    
+
