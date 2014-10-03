3253254179dd12f7b1303b1d175de097fc8ef079
car517 <car517@lehigh.edu>
2014-10-03 03:21:26 +0000

    Updated Corrected BurgerKing and Boola Boola

--- hw05/BurgerKing.java
+++ hw05/BurgerKing.java
@@ -7,19 +7,21 @@
 // We want to users to enter a choice of a burger soda and fries
 
 import java.util.Scanner;
-Scanner myScanner; 
 
 public class BurgerKing {
+
+static Scanner myScanner;
+
     public static void main(String[] args) {
         myScanner= new Scanner (System.in);
         System.out.println(" Enter the first letter of your food of choice. \nYour choices are: Burger (B or b) , Soda (S or s) , Fries (F or f) , All the fixens (A) ");
         String Foodltr="o"; String Food="o";
-        Foodltr= myScanner.Next();
-        
-        switch (Foodltr.lowerCase()) {
-            case "b": 
+        Foodltr= myScanner.next();
+
+        switch ( Foodltr.toString().toLowerCase() ) {
+            case "b":
                 System.out.println("You have selected a burger, would you like cheese (Y, N) ? ");
-                    Food= myScanner.Next(); 
+                    Food= myScanner.next();
                     if (Food.equals("y") || Food.equals("Y") || Food.equals("N") || Food.equals("n") ) {
                         if (Food.equals("y") || Food.equals("Y") ) {
                             System.out.println("You have ordered a Cheeseburger");
@@ -34,7 +36,7 @@ public class BurgerKing {
                     break;
             case "f":
                 System.out.println("You have selected fries, would you like a large or small (l , s)  ?");
-                    Food= myScanner.Next();
+                    Food= myScanner.next();
                     if (Food.equals("s") || Food.equals("S") || Food.equals("l") || Food.equals("L") ) {
                         if (Food.equals("s") || Food.equals("S") ) {
                             System.out.println("You have ordered small fries");
@@ -47,9 +49,9 @@ public class BurgerKing {
                         System.out.println("You have not entered a proper entry");
                     }
                     break;
-            case "s":
+            case "s": 
                 System.out.println("Would you like pepsi (p or P), coke (c or C), Mountatin Dew (M or m) ?");
-                    Food= myScanner.Next();
+                    Food= myScanner.next();
                     if (Food.equals("p") || Food.equals("P") || Food.equals("c") || Food.equals("C") || Food.equals("M") || Food.equals("m") ) {
                         if (Food.equals("p") || Food.equals("P") ) {
                             System.out.println("You have ordered a Pepsi");
@@ -65,11 +67,10 @@ public class BurgerKing {
                         System.out.println("You have not entered a proper entry");
                     }
                     break;
-            default: 
+            default:
                 System.out.println("You did not enter a valid entry");
                     break;
         }
-        
+
     }
 }
-
